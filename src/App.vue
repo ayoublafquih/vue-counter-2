0e23cd341fd434957fb6032d098b96b7db2c655c
ayoublafquih <39165054+ayoublafquih@users.noreply.github.com>
2023-12-15 11:46:00 +0000

    add repo

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      addToCount() {
+        this.count++;
+      },
+      substructFromCount() {
+        this.count--;
+      }
+    }
+}
+</script>
+<template>
+  <main>
+    <div>
+      <h4>The current count is ...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="substructFromCount">-</button>
+      <button @click="addToCount">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
